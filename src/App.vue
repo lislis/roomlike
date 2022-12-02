34d1c52fa90b1b9145ab20d28af388186ba67bd4
lislis <mail@lislis.de>
2022-12-02 15:41:49 +0100

    finish

--- src/App.vue
+++ src/App.vue
@@ -2,47 +2,70 @@
     <header><h1>Roomlike</h1></header>
     <main>
         <div>
-            <h2>Item pool</h2>
+            <h2>Define items</h2>
             <p>List available items here.
                 <br>One item per line.
                 <br>Two items per line, separated by comma, mean they are dependend on each other and will both appear in the room, but individually.</p>
+
                 <textarea :value="itemsRaw"
                           @change="update"></textarea>
                 <button type="button">Parse input</button>
         </div>
 
-        <div>
-            <h2>Available Items</h2>
+        <div v-if="items.length > 0">
+            <h2>Room Settings</h2>
+
+            <p>Available items. Items on the same line will always be put in together.</p>
             <ul v-if="items.length">
                 <li v-for="item in items">{{ item }}</li>
             </ul>
+
+            <label>Number of items for solution (could be +1 depending on items)
+                <input type="number" v-model="numSolutionItems">
+            </label>
+            <label>Max number of items in the room
+                <input type="number" v-model="numMaxItems">
+            </label>
+            <label>Max number of tries
+                <input type="number" v-model="numMaxTries">
+            </label>
             <button type="button" @click="generateRoom">Generate room!</button>
         </div>
 
         <div v-if="solutionItems.size > 0">
-            <h2>Room generated</h2>
-            <div class="blockies">
+            <h2>The Room</h2>
+            <div class="room">
+                <div v-for="x in roomItems" @click="tryItem" class="item">{{x}}</div>
+            </div>
+
+            <div v-if="!gameover && !win" class="blockies">
+                <p>Solution:</p>
                 <div v-for="x in solutionItems" class="blocky"></div>
             </div>
 
             <div v-if="tryHistory.length > 0">
-                <p>try history</p>
+                <p>Try history ({{ tryHistory.length}} / {{ numMaxTries }})</p>
                 <ul>
-                <li v-for="t in tryHistory" >
-                    <div v-for="item in t" class="item" :class="item.color">{{item.item}}</div>
-                </li>
+                    <li v-for="t in tryHistory" >
+                        <div v-for="item in t" class="item" :class="item.color">{{item.item}}</div>
+                    </li>
                 </ul>
             </div>
 
             <div v-if="currentItems.size > 0" class="blockies">
-                <p>current try: </p>
-                <div v-for="x in currentItems" class="">{{ x }}</div>
+                <p>Current try: </p>
+                <div v-for="x in currentItems" class="try-blocky">{{ x }}</div>
             </div>
-            <div class="room">
-                <div v-for="x in solutionItems" @click="tryItem" class="item">{{x}}</div>
 
-
+            <div v-if="gameover">
+                <h1>GAME OVER</h1>
+                <p>Generate a new room to start over.</p>
             </div>
+            <div v-if="win">
+                <h1>YOU WIN</h1>
+                <p>Good job.</p>
+            </div>
+
         </div>
     </main>
 </template>
@@ -55,34 +78,73 @@
              itemsRaw: '',
              solutionItems: new Set(),
              currentItems: new Set(),
-             tryHistory: []
+             roomItems: new Set(),
+             tryHistory: [],
+             numSolutionItems: 3,
+             numMaxItems: 7,
+             numMaxTries: 5,
+             gameover: false,
+             win: false
          }
      },
      methods: {
          update(evt) {
              this.itemsRaw = evt.target.value;
          },
+         shuffleArray(arr) {
+             arr.sort(() => Math.random() - 0.5);
+         },
          generateRoom() {
+             this.gameover = false;
+             this.win = false;
              this.tryHistory = [];
              this.solutionItems = new Set();
              this.currentItems = new Set();
+             let interrim = new Set();
+             this.roomItems = new Set();
 
-             while (this.solutionItems.size <= 2) {
-                 let newItem = this.items[Math.floor(Math.random()*this.items.length)];
-                 if (newItem.length > 1) {
-                     newItem.forEach(x => this.solutionItems.add(x))
-                 } else {
-                     this.solutionItems.add(newItem[0])
+             if (this.numSolutionItems <= this.items.length) {
+                 while (this.solutionItems.size < this.numSolutionItems) {
+                     let newItem = this.items[Math.floor(Math.random()*this.items.length)];
+                     if (newItem.length > 1) {
+                         newItem.forEach(x => {
+                             this.solutionItems.add(x);
+                             interrim.add(x)
+                         })
+                     } else {
+                         this.solutionItems.add(newItem[0])
+                         interrim.add(newItem[0])
+                     }
+                 }
+             } else {
+                 console.log("not enough items")
+             }
+
+             if (this.numMaxItems <= this.items.length) {
+                 while (interrim.size < this.numMaxItems) {
+                     let newItem = this.items[Math.floor(Math.random()*this.items.length)];
+                     if (newItem.length > 1) {
+                         newItem.forEach(x => {
+                             interrim.add(x)
+                         })
+                     } else {
+                         interrim.add(newItem[0])
+                     }
                  }
 
-                 //this.solutionItems.add(newItem);
+                 let arr = Array.from(interrim);
+                 this.shuffleArray(arr);
+                 this.roomItems = new Set(arr);
+             } else {
+                 console.log("not enough items")
              }
          },
          tryItem(evt) {
-             //console.log(evt.target.textContent)
-             this.currentItems.add(evt.target.textContent);
-             if (this.currentItems.size === this.solutionItems.size) {
-                 this.checkSolution();
+             if (!this.gameover && !this.win) {
+                 this.currentItems.add(evt.target.textContent);
+                 if (this.currentItems.size === this.solutionItems.size) {
+                     this.checkSolution();
+                 }
              }
          },
          checkSolution() {
@@ -90,20 +152,29 @@
              const solution = Array.from(this.solutionItems);
 
              let outcome = current.map((c, i) => {
-                 //debugger
                  if (c === solution[i]) {
                      return { item: c, color: 'green'}
                  }
                  if (solution.indexOf(c) != -1) {
                      return { item: c, color: 'yellow'}
-                     //return 'yellow'
                  } else {
                      return { item: c, color: 'black'}
-                     //                     return 'black'
                  }
              });
+             this.checkWin(outcome);
              this.tryHistory.push(outcome);
              this.currentItems = new Set();
+             this.checkTries();
+         },
+         checkWin(arr) {
+             if (arr.filter(x => x.color === 'green').length === this.solutionItems.size) {
+                 this.win = true
+             }
+         },
+         checkTries() {
+             if (this.tryHistory.length === this.numMaxTries) {
+                 this.gameover = true
+             }
          }
      },
      computed: {
@@ -119,44 +190,3 @@
      }
  }
 </script>
-
-<style>
- main {
-     display: flex;
-     flex-flow: row nowrap;
- }
- main > div {
-     flex: 1 1 30%;
-     padding: 0 2em;
-     border-right: 2px currentColor dotted;
- }
- textarea {
-     display: block;
- }
- .blocky {
-     width: 1em;
-     height: 1em;
-     display: inline-block;
-     margin-right: 0.2em;
-     background-color: black;
- }
-
- .room {
-     background-color: lightgrey;
-     padding: 1em;
- }
-
- .item {
-     display: inline-block;
-     cursor: pointer;
-     padding: 0.2em;
-     margin-right: 0.2em;
-     background-color: grey;
- }
- .item.green {
-     background-color: green
- }
- .item.yellow {
-     background-color: yellow
- }
-</style>
